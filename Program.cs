d43d5b728e0817189fcc293837c9ee5929e04f1e
KyleCSharp <kylelrigler@gmail.com>
2022-08-20 01:20:03 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             string myName = "Kyle";
+              int myAge = 23;
+            char myCharacter = '$';
+            bool isBoolen = false;
+            decimal myDecimal = 50000000000.25m;
+            double myDouble = -345.28;
+            Console.WriteLine($"my name is {myName},I am {myAge} years old ...my goal in life is to make {myCharacter} and be successful and nothing els matters to me ..this is {isBoolen}.A happy life, family, knowledge, and {myCharacter} matter to me ");
+
+            Console.WriteLine($"honestly I could not think of how to impliment double and decimal into a mini story so here they are{myDecimal}");
+            Console.WriteLine(myDouble);
+            Console.WriteLine($"Hello,Truecoders!!!");
+            /*Please send me a message
+            on discord for feedback.*/
+        }
+    }
+}
